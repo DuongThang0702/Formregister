1a357e6a959ef8f8c65595b52a179abb4651ea2a
Duong Thang <duongthang0702@gmail.com>
2023-10-29 16:25:08 +0700

    ui use camera

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,33 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { FC } from "react";
+import style from "@/styles/pages/_homepage.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import icon from "@/utils/icon";
 
-export default function Home() {
+const Page: FC = ({}) => {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={style.container}>
+      <div className={style.top}>
+        <FontAwesomeIcon icon={icon.faFileImage} className={style.icon} />
+        <div className={style.container_text}>
+          <p>You need to Upload your</p>
+          <span>Form register</span>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={style.button}>
+        <FontAwesomeIcon icon={icon.faCamera} className={style.icon} />
+        <p>Use Camera</p>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={style.bottom}>
+        <div className={style.container_bottom}>
+          <FontAwesomeIcon icon={icon.faImage} className={style.icon} />
+          <div>
+            <span>Select the documents from Gallery</span>
+            <p>PNG_JPEG</p>
+          </div>
+        </div>
       </div>
-    </main>
-  )
-}
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/utils/icon.ts
@@ -0,0 +1,6 @@
+import { faFileImage, faImage } from "@fortawesome/free-regular-svg-icons";
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
+
+const icon = { faFileImage, faImage, faCamera };
+
+export default icon;
