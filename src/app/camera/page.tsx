3df651259a7b340fc1a6f7ffa06535ebba0ccc0b
Duong Thang <duongthang0702@gmail.com>
2023-10-29 21:15:42 +0700

    test

--- src/app/camera/page.tsx
+++ src/app/camera/page.tsx
@@ -9,45 +9,61 @@ import icon from "@/utils/icon";
 import { Routes } from "@/utils/path";
 import button from "@/styles/components/_button.module.scss";
 
-const videoConstraints = {
-  width: 500,
-  height: 450,
-  facingMode: "user",
-};
+const elem =
+  typeof window !== "undefined" &&
+  window.document &&
+  document.compatMode === "CSS1Compat"
+    ? document.documentElement
+    : document.body;
 
 const Page: FC = ({}) => {
   const router = useRouter();
   const webRef = useRef<Webcam>(null);
   const [img, setImage] = useState<string | null>(null);
+
   const showImage = () => {
     const Image = webRef.current?.getScreenshot();
     if (Image) setImage(Image);
   };
 
+  const videoConstraints = {
+    facingMode: "user",
+    width: elem.clientWidth,
+    height: elem.clientHeight,
+  };
+  useEffect(() => {
+    const elem =
+      typeof window !== "undefined" &&
+      window.document &&
+      document.compatMode === "CSS1Compat"
+        ? document.documentElement
+        : document.body;
+  }, []);
+
   return (
-    <div className={style.wapper}>
-      <div className={style.container}>
-        <div className={style.header}>
+    <div className={style.container}>
+      <div className={style.header}>
+        <FontAwesomeIcon
+          icon={icon.faArrowLeftLong}
+          onClick={() => router.push(`${Routes.HOME_PAGE}`)}
+        />
+      </div>
+      <div className={style.camera}>
+        <Webcam
+          ref={webRef}
+          audio={false}
+          screenshotFormat="image/jpeg"
+          videoConstraints={videoConstraints}
+          width={videoConstraints.width}
+          height={videoConstraints.height - 100}
+          mirrored={true}
+        />
+        <div className={style.container_button}>
           <FontAwesomeIcon
-            icon={icon.faArrowLeftLong}
-            onClick={() => router.push(`${Routes.HOME_PAGE}`)}
+            icon={icon.faCircle}
+            className={button.circleCameraButton}
+            onClick={() => showImage()}
           />
-        </div>
-        <div className={style.camera}>
-          <Webcam
-            ref={webRef}
-            audio={false}
-            screenshotFormat="image/jpeg"
-            videoConstraints={videoConstraints}
-            mirrored={true}
-          />
-          <div className={style.container_button}>
-            <FontAwesomeIcon
-              icon={icon.faCircle}
-              className={button.circleCameraButton}
-              onClick={() => showImage()}
-            />
-          </div>
         </div>
       </div>
     </div>
