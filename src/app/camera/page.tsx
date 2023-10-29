1646b5ffc04cb62f4b7e33f740640da81b44ee0c
Duong Thang <duongthang0702@gmail.com>
2023-10-29 18:10:27 +0700

    fix size

--- src/app/camera/page.tsx
+++ src/app/camera/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import style from "@/styles/pages/_camerapage.module.scss";
 import Webcam from "react-webcam";
-import { FC, useRef, useState } from "react";
+import { FC, useRef, useState, useEffect } from "react";
 import { Camera } from "@/components";
 import { useRouter } from "next/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -9,33 +9,54 @@ import icon from "@/utils/icon";
 import { Routes } from "@/utils/path";
 
 const videoConstraints = {
-  width: 390,
-  height: 700,
-  facingMode: { exact: "environment" },
+  width: 400,
+  height: 646,
+  facingMode: "user",
 };
 
 const Page: FC = ({}) => {
+  const a = usewindowsize();
   const router = useRouter();
   const webRef = useRef<Webcam>(null);
   const [img, setImage] = useState<string | null>(null);
 
   return (
     <div className={style.container}>
-      <div
-        className={style.header}
-        onClick={() => router.push(`${Routes.HOME_PAGE}`)}
-      >
-        <FontAwesomeIcon icon={icon.faArrowLeftLong} />
-      </div>
+      <FontAwesomeIcon icon={icon.faArrowLeftLong} />
       <Camera
-        height={700}
+        height={a.height !== undefined ? a.height - 90 : 1}
+        width={a.width !== undefined ? a.width : 1}
         webRef={webRef}
         videoConstraints={videoConstraints}
-        width={390}
         setImage={setImage}
       />
     </div>
   );
 };
+
+function usewindowsize() {
+  const [windowsize, setwindowsize] = useState<{
+    width: undefined | number;
+    height: undefined | number;
+  }>({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const handleresize = () => {
+        setwindowsize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      };
+      window.addEventListener("resize", handleresize);
+      handleresize();
+      return () => window.removeEventListener("resize", handleresize);
+    }
+  }, []);
+  return windowsize;
+}
 
 export default Page;
