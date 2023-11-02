2e7a7fa41784099d62ed47a19e06ffe760956640
Duong Thang <duongthang0702@gmail.com>
2023-11-02 09:50:06 +0700

    update meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </head>
       <body suppressHydrationWarning={true}>{children}</body>
     </html>
   );
